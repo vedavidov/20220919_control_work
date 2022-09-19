934ddebb0cc5ae63effcf62c9c45ecd7be52c726
Viacheslav <davidov.ve@yandex.ru>
2022-09-19 10:20:31 +0300

    Added prgram project

---
+++ mean_4_num/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа принимает на вход 4 числа и считает среднее арифметическое значение.
+
+Console.WriteLine("Введите чилсо a: ");
+double a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите чилсо b: ");
+double b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите чилсо c: ");
+double c = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите чилсо d: ");
+double d = int.Parse(Console.ReadLine());
+
+double mean = 0;
+mean = (a + b + c + d) / 4;
+
+Console.WriteLine($"Среднее арифметическое значение: {mean,2}");
