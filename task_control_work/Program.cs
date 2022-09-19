83bab2f6b35e8b2d28746966e01b1f23febd56e3
Viacheslav <davidov.ve@yandex.ru>
2022-09-19 10:56:33 +0300

    Task control work project

---
+++ task_control_work/Program.cs
@@ -0,0 +1,50 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам.
+
+int[,] matrix = new int[3, 3];
+
+FillArray(matrix);
+Console.WriteLine("Исходный массив: ");
+PrintArray(matrix);
+Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            if (matrix[i, k] < matrix[i, k + 1])
+            {
+                int number = 0;
+                number = matrix[i, k];
+                matrix[i, k] = matrix[i, k + 1];
+                matrix[i, k + 1] = number;
+            }
+        }
+    }
+}
+Console.WriteLine("Новый массив: ");
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 1000); // [1, 10)
+        }
+    }
+}
